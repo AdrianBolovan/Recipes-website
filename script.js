7b9394d40e3f202575e7299d2ecd37df75d83d3f
AdrianBolovan <adrian_bolovan@yahoo.com>
2026-02-08 14:00:47 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMobileNav() {
+  let mobileNav = document.getElementById("mobile_nav");
+  let burgerBtn = document.getElementById("burger_btn");
+  let closeBtn = document.getElementById("close_btn");
+
+  mobileNav.classList.toggle("d-none");
+  
+  if (!mobileNav.classList.contains("d-none")) {
+    burgerBtn.classList.add("d-none");
+    closeBtn.classList.remove("d-none");
+  } else {
+    burgerBtn.classList.remove("d-none");
+    closeBtn.classList.add("d-none");
+  }
+
+  mobileNav.classList.
+}
